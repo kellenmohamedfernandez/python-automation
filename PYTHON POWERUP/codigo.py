90a9a0b170aec26562e8502610092549a0d5bb66
Hitss <Hitss@RJONB008274.globalhitss.com.br>
2026-01-12 20:32:13 -0300

    Primeiros comandos pyautogui

---
+++ PYTHON POWERUP/codigo.py
@@ -0,0 +1,27 @@
+# bibliotecas -> pip install pyautogui
+
+import pyautogui
+import time
+
+# pyautogui.click -- click do mouse
+# pyautogui.write -- escreve um texto
+# pyautogui.press -- aperta o enter
+# pyautogui.hotkey -- aperta um atalho
+
+pyautogui.PAUSE = 0.5 # Espera para executar os comandos
+
+url = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
+
+# Comandos para abrir o navegador:
+pyautogui.press("win") #tecla windows
+pyautogui.write("chrome")
+pyautogui.press("enter")
+pyautogui.write(url)
+pyautogui.press("enter")
+time.sleep(3)
+
+# Preenchendo os campos Email e Senha:
+pyautogui.click(x=674, y=447)
+pyautogui.write("test@test.com")
+
+
